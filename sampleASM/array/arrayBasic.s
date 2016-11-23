22f13a181663d16f0c24fed98aa313b5e4230721
Tanmay <tk915@ic.ac.uk>
2016-11-23 11:25:41 +0000

    [TK] Updated sampleASM directory for easier testing

---
+++ sampleASM/array/arrayBasic.s
@@ -0,0 +1,23 @@
+valid/array/arrayBasic.wacc
+calling the reference compiler on valid/array/arrayBasic.wacc
+-- Test: arrayBasic.wacc
+
+-- Uploaded file: 
+---------------------------------------------------------------
+# basic array declaration and assignment
+
+# Output:
+# #empty#
+
+# Program:
+
+begin
+  int[] a = [0]
+end
+
+---------------------------------------------------------------
+
+-- Compiler Output:
+-- Compiling...
+-- Finished
+
